e94cd9ce77a3d072f4e880a1639d3355af0f60c1
Arum Puspa A <92013755+arumpuspaarianto@users.noreply.github.com>
2021-10-19 18:43:41 +0700

    Add files via upload

--- _layouts/head.php
+++ _layouts/head.php
@@ -2,19 +2,24 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
-    <title> <?php echo $title ?> | Hospital Management System</title>
+    <title>Hospital Management System</title>
     <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?=templates()?>images/favicon.png">
     <!-- Nestable -->
     <link href="<?=templates()?>vendor/nestable2/css/jquery.nestable.min.css" rel="stylesheet">
     <!-- Custom Stylesheet -->
-    <link href="<?=templates()?>vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
-    <link rel="icon" type="image/png" sizes="16x16" href="<?=templates()?>images/favicon.png">
-    <link href="<?=templates()?>vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
-	<link rel="stylesheet" href="<?=templates()?>vendor/chartist/css/chartist.min.css">
+    	<link href="<?=templates()?>vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
     <link href="<?=templates()?>css/style.css" rel="stylesheet">
-	<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
-    <link href="<?=templates()?>vendor/datatables/css/jquery.dataTables.min.css" rel="stylesheet">
-
     <script src="https://kit.fontawesome.com/f4559b5252.js" crossorigin="anonymous"></script>
+
+
+    <link rel="icon" type="image/png" sizes="16x16" href="./images/favicon.png">
+    <link href="./vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="./vendor/chartist/css/chartist.min.css">
+    <link href="./vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
+    <link href="./css/style.css" rel="stylesheet">
+	<link href="https://cdn.lineicons.com/2.0/LineIcons.css" rel="stylesheet">
+
 </head>
+
+
--- _layouts/javascript.php
+++ _layouts/javascript.php
@@ -1,34 +1,10 @@
-	<!-- Dashboard 1 -->
-	<script src="<?=templates()?>js/dashboard/dashboard-1.js"></script>
-
-	<!-- Required vendors -->
-    <script src="<?=templates()?>vendor/global/global.min.js"></script>
+<script src="<?=templates()?>vendor/global/global.min.js"></script>
 	<script src="<?=templates()?>vendor/bootstrap-select/dist/js/bootstrap-select.min.js"></script>
-    <script src="<?=templates()?>vendor/chart.js/Chart.bundle.min.js"></script>
     <script src="<?=templates()?>js/custom.min.js"></script>
 	<script src="<?=templates()?>js/deznav-init.js"></script>
 	<!-- Apex Chart -->
 	<script src="<?=templates()?>vendor/apexchart/apexchart.js"></script>
 	
-	<!-- Chartist -->
-    <script src="<?=templates()?>vendor/chartist/js/chartist.min.js"></script>
-    <script src="<?=templates()?>vendor/chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js"></script>
-    
-	<!-- Flot -->
-    <script src="<?=templates()?>vendor/flot/jquery.flot.js"></script>
-    <script src="<?=templates()?>vendor/flot/jquery.flot.pie.js"></script>
-    <script src="<?=templates()?>vendor/flot/jquery.flot.resize.js"></script>
-    <script src="<?=templates()?>vendor/flot-spline/jquery.flot.spline.min.js"></script>
 	
-	<!-- Chart sparkline plugin files -->
-    <script src="<?=templates()?>vendor/jquery-sparkline/jquery.sparkline.min.js"></script>
-	<script src="<?=templates()?>js/plugins-init/sparkline-init.js"></script>
-	
-	<!-- Chart piety plugin files -->
-    <script src="<?=templates()?>vendor/peity/jquery.peity.min.js"></script>
-	<script src="<?=templates()?>js/plugins-init/piety-init.js"></script>
-	
-    <!-- Init file -->
-    <script src="<?=templates()?>js/plugins-init/widgets-script-init.js"></script>
-    <script src="<?=templates()?>vendor/datatables/js/jquery.dataTables.min.js"></script>
-    <script src="<?=templates()?>js/plugins-init/datatables.init.js"></script>
+	<!-- Dashboard 1 -->
+	<script src="<?=templates()?>js/dashboard/dashboard-1.js"></script>
