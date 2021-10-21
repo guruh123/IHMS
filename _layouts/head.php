1f7710df8dc6c73990ceeb57fb4209506297e6f0
kamil265 <m.insanulkamil26@gmail.com>
2021-10-21 07:58:12 +0700

    integrasi halaman pasien

--- _layouts/head.php
+++ _layouts/head.php
@@ -3,11 +3,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width,initial-scale=1">
     <title> <?php echo $title ?> | Hospital Management System</title>
-    <!-- Favicon icon -->
     <link rel="icon" type="image/png" sizes="16x16" href="<?=templates()?>images/favicon.png">
-    <!-- Nestable -->
     <link href="<?=templates()?>vendor/nestable2/css/jquery.nestable.min.css" rel="stylesheet">
-    <!-- Custom Stylesheet -->
     <link href="<?=templates()?>vendor/bootstrap-select/dist/css/bootstrap-select.min.css" rel="stylesheet">
     <link rel="icon" type="image/png" sizes="16x16" href="<?=templates()?>images/favicon.png">
     <link href="<?=templates()?>vendor/jqvmap/css/jqvmap.min.css" rel="stylesheet">
